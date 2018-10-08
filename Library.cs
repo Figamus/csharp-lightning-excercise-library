144ce3d3e876bd0a43af664f2b3232c4e0bf06dc
Figamus <fig74656@yahoo.com>
2018-10-08 11:05:21 -0500

    Made the Library work with constructor

---
+++ Library.cs
@@ -0,0 +1,30 @@
+using System.Collections.Generic;
+
+namespace Library
+{
+    public class Library
+    {
+        // Constructor
+        public Library(List<Book> initialInventory, string name, string address)
+        {
+            _bookList = initialInventory;
+            Name = name;
+            Address = address;
+        }
+        // Properties
+        public string Address { get; set; }
+        public string Name { get; set; }
+        private List<Book> _bookList = new List<Book>();
+        // or
+        // private List<Book> _bookList;
+
+        // Methods
+        // Methods has to pass the TYPE of object and the variable name
+        public void AddToInventory (Book book)
+        {
+            _bookList.Add(book);
+            // or use 'this' to look within this specific class
+            // this._bookList.Add(book);
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using Library;
+
+namespace Library
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Create a few (4 or so) instances of books and use the method on your Library class to add them to the list of books in an instance of Library
+
+            // Creating a list of book to pass into the library constructor
+            Book book1 = new Book(){
+                Title = "a",
+                Author = "a",
+                ISBN = "a",
+            };
+            Book book2 = new Book(){
+                Title = "b",
+                Author = "b",
+                ISBN = "b",
+            };
+            List<Book> books = new List<Book>(){
+                book1, book2
+            };
+
+            // Create a new library with a premade list of books for it's contructor
+            Library NewLibrary = new Library(books, "Downtown Library", "123 Some St. Nashville, TN 12345");
+            // before contructor
+            // Library NewLibrary = new Library(){
+            //     Name = "Downtown Library",
+            //     Address = "Someplace Downtown"
+            // };
+
+            // creating books
+            Book endersGame = new Book() {
+                Title = "Ender's Game",
+                Author = "Orsen Scott",
+                ISBN = "ABC123"
+            };
+            Book lotr = new Book() {
+                Title = "Lord of the Rings",
+                Author = "J R R Tolkein",
+                ISBN = "ABC125"
+            };
+            // Alternate way to make a book object
+            Book tomSawyer = new Book();
+                tomSawyer.Title = "Ender's Game";
+                tomSawyer.Author = "Orsen Scott";
+                tomSawyer.ISBN = "ABC124";
+            // or this way, but you don't get to hold the books in a variable name.
+            // NewLibrary.AddToInventory(new Book() {Title = "Something", Author = "Some Dude", ISBN = "A0adf934"});
+            // NewLibrary.AddToInventory(new Book() {Title = "Something Else", Author = "Some Other Dude ", ISBN = "sadgfsdgf34"});
+            // NewLibrary.AddToInventory(new Book() {Title = "Something More", Author = "AnotherDude", ISBN = "sadfasdgf5676756"});
+            // NewLibrary.AddToInventory(new Book() {Title = "Bob", Author = "Bob", ISBN = "B0B"});
+        }
+    }
+}
